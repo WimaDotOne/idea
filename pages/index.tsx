a4adf62618b000103310d284f5c16b4ce9a644ae
Fan <36140830+fan2015zheng@users.noreply.github.com>
2022-02-28 10:47:12 -0600

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (<>
+    <Head>
+      <title>Ideas</title>
+      <meta name="description" content="Web app ideas" />
+      <link rel="icon" href="/img/favicon.ico" />
+    </Head>
+    Home
+  </>)
+}
+
+export default Home
