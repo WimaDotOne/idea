04d7326afa981383c73e357cc58d5a494b29367f
Fan <36140830+fan2015zheng@users.noreply.github.com>
2022-03-01 09:46:17 -0600

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { Div } from '../ideas/Core/fCore'
+import cl from "./index.module.scss"
 
 const Home: NextPage = () => {
   return (<>
@@ -8,7 +11,23 @@ const Home: NextPage = () => {
       <meta name="description" content="Web app ideas" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    Home
+    
+    <Div padding={20} paddingTop={0}>
+      <h3>Sketchbook</h3>
+      <div className={cl.links}>
+      <Link href="/AppleWindow"><a>Apple Window</a></Link> &nbsp;&nbsp;
+      <Link href="/GoogleForm"><a>Google Form</a></Link> &nbsp;&nbsp;
+      <Link href="/StripeLogin"><a>Stripe Login</a></Link> &nbsp;&nbsp;
+      <Link href="/StripeLogin/Privacy"><a>Privacy</a></Link> &nbsp;&nbsp;
+      <Link href="/StripeLogin/Terms"><a>Terms</a></Link> &nbsp;&nbsp;
+      <Link href="/StripeLogin/Contact"><a>Contact</a></Link> &nbsp;&nbsp;
+      </div>
+      <Div height={20} />
+      <h3>Ideas</h3>
+      <div className={cl.links}>
+      <Link href="/RosettaStone"><a>Rosetta Stone</a></Link> &nbsp;&nbsp;
+      </div>
+   </Div>
   </>)
 }
 
